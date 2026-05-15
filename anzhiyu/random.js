b1772c36d6de54d905c58d3708be8f7f826f48c5
duan <1136168563@qq.com>
2026-05-16 02:48:43 +0800

    Site updated: YYYY-05-2026年5月16日 02:48:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/16/啦啦啦/","2026/05/16/进来一个揍一个/"];function toRandomPost(){
+var posts=["2026/05/16/啦啦啦/","2026/05/16/我终于弄好啦/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
